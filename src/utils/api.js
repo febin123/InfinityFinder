652c34afdaf61e622bd77d301861390239f124b2
febin123 <febsep96@gmail.com>
2024-04-27 19:18:45 +0530

    added API key

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,14 @@
 import axios from "axios";
+const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 
-export const fetchDataFromApi = () => {};
+const params = {
+    key: "AIzaSyBg3qozwqgWbx1YrfkLoKPXzDEHCzp3FZ8",
+    cx: "8390aadd287f74eba",
+};
+
+export const fetchDataFromApi = async (payload) => {
+    const { data } = await axios.get(BASE_URL, {
+        params: { ...params, ...payload },
+    });
+    return data;
+};;
